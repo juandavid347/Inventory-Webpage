1f1a8c634888ebf630322d7c474e6e5f942374b5
Juan D. Elizabeth <juandavid347@hotmail.com>
2023-02-14 13:36:11 -0600

    Added Items to Database

---
+++ Data/Migrations/20230214193408_items.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class items : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Company = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    ItemID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Category = table.Column<string>(type: "TEXT", nullable: true),
+                    PartNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    SerialNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    Units = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Quantity = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.ItemID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SaleOrder",
+                columns: table => new
+                {
+                    SaleID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CustomerID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SaleOrder", x => x.SaleID);
+                    table.ForeignKey(
+                        name: "FK_SaleOrder_Customer_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SaleItems",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ItemID = table.Column<int>(type: "INTEGER", nullable: false),
+                    SaleID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<uint>(type: "INTEGER", nullable: false),
+                    SaleOrderSaleID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SaleItems", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_SaleItems_Item_ItemID",
+                        column: x => x.ItemID,
+                        principalTable: "Item",
+                        principalColumn: "ItemID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SaleItems_SaleOrder_SaleOrderSaleID",
+                        column: x => x.SaleOrderSaleID,
+                        principalTable: "SaleOrder",
+                        principalColumn: "SaleID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleItems_ItemID",
+                table: "SaleItems",
+                column: "ItemID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleItems_SaleOrderSaleID",
+                table: "SaleItems",
+                column: "SaleOrderSaleID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleOrder_CustomerID",
+                table: "SaleOrder",
+                column: "CustomerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SaleItems");
+
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.DropTable(
+                name: "SaleOrder");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
